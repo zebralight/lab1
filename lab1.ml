e2ffef073fee0eecb99475a269cd3c44e30ee6c5
quilty <quiltykim1@gmail.com>
2018-12-30 14:46:03 -0800

    complete square all and sum

--- lab1.ml
+++ lab1.ml
@@ -167,18 +167,22 @@ to the list containing the elements 3, 4, and 5? You'll want to
 replace the "[]" with the correct functional call.
 ......................................................................*)
 
-let square_all (lst : int list) : int list =
-  failwith "square_all not implemented" ;;
+let rec square_all (lst : int list) : int list =
+  match lst with
+    | [] -> []
+    | x::lst' -> x*x :: square_all lst'
 
-let exercise6 = [] ;;
+let exercise6 = square_all [3; 4; 5] ;;
 
 (*......................................................................
 Exercise 7: Define a recursive function that sums an integer
 list. (What's a sensible return value for the empty list?)
 ......................................................................*)
 
-let sum (lst : int list) : int =
-  failwith "sum not implemented" ;;
+let rec sum (lst : int list) : int =
+  match lst with
+    | [] -> 0
+    | x::lst' -> x + sum lst'
   
 (*......................................................................
 Exercise 8: Define a recursive function that returns the maximum
